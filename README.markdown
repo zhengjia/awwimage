6f8eb5aecbbf1e373c84614dd49b32cbe4a8b6a2
zhengjia <jiazheng@live.com>
2013-05-05 20:39:46 -0500

    add

---
+++ README.markdown
@@ -0,0 +1,1 @@
+heroku config:add BUILDPACK_URL=https://github.com/kr/heroku-buildpack-go.git
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import(
   "fmt"
 	"net/http"
+	"os"
 )
 
 type ImagemiServer struct{}
@@ -12,6 +13,10 @@ func (h ImagemiServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    var port = os.Getenv("PORT")
+    if port == "" {
+      port = "4000"
+    }
     var h ImagemiServer
-    http.ListenAndServe("localhost:4000", h)
+    http.ListenAndServe(":" + port, h)
 }
