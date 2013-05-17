0b6eb977488ba271f32dcd96a5a67f01e21e2b6d
zhengjia <jiazheng@live.com>
2013-05-16 23:26:21 -0500

    Revert "app engine files"
    This reverts commit eca32bb48ab8f84a9ae96618c9604314698d2d09.

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestEndpointsContainAllRoutes(t *testing.T) {
   var endpoints = *Endpoints()
-  var routes = []string{"/instruction", "/count/:keyword", "/random/:keyword", "/bomb/:keyword/:number"}
+  var routes = []string{"/instruction", "/count/:keyword", "/random/:keyword", "/bomb/:keyword/:number", "/all/:keyword"}
   for _, route := range routes {
     if _, ok := endpoints[route] ; !ok {
       t.Errorf("Expected key %s", route)
