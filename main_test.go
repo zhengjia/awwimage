efade055116a1bade47b46959ad9e1d98f0ed176
zhengjia <jiazheng@live.com>
2013-05-17 11:52:40 -0500

    goformat

--- main_test.go
+++ main_test.go
@@ -3,18 +3,18 @@ package main
 import "testing"
 
 func TestEndpointsContainAllRoutes(t *testing.T) {
-  var endpoints = *Endpoints()
-  var routes = []string{"/instruction", "/count/:keyword", "/random/:keyword", "/bomb/:keyword/:number"}
-  for _, route := range routes {
-    if _, ok := endpoints[route] ; !ok {
-      t.Errorf("Expected key %s", route)
-    }
-  }  
+	var endpoints = *Endpoints()
+	var routes = []string{"/instruction", "/count/:keyword", "/random/:keyword", "/bomb/:keyword/:number"}
+	for _, route := range routes {
+		if _, ok := endpoints[route]; !ok {
+			t.Errorf("Expected key %s", route)
+		}
+	}
 }
 
 func TestGetJsonString(t *testing.T) {
-	json := GetJsonString(&map[string][]string{"urls": []string{"b", "a"}} )
+	json := GetJsonString(&map[string][]string{"urls": []string{"b", "a"}})
 	if json != "{\"urls\":[\"b\",\"a\"]}" {
-	  t.Errorf("Returned %s", json)
+		t.Errorf("Returned %s", json)
 	}
 }
